44da8c900d2399d8fa4f9b81f85f24dae85f1586
Chinmay Bhoir <bhoirchinmay2014@gmail.com>
2022-04-30 17:30:15 +0530

    Added Files

--- Other/testing_task3/main2.cc
+++
@@ -1,9 +0,0 @@
-#include "BMP.h"
-#include<iostream>
-
-int main(){
-	// Read an image from disk,modify it and write it back
-	BMP bmp("bmp_file.bmp");
-	bmp.write("bmp_file_copy.bmp");
-	return 0;
-}
